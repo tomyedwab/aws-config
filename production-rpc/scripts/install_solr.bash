257b23167ef3c7c6146bca70b7a22ab0e8561f19
Tom W. Yedwab <tom@khanacademy.org>
2012-05-15 19:02:49 -0700

    Fixes from Github pull request.

--- production-rpc/scripts/install_solr.bash
+++ production-rpc/scripts/install_solr.bash
@@ -3,9 +3,9 @@
 # This script installs Tomcat and Solr/Lucene for search indexing of the
 # KhanAcademy.org content.
 
-# This script assumes an Ubuntu 11.10 server and home directory /home/ubuntu.
-# This git repository should be cloned into /home/ubuntu/aws-config.
-export REPO_ROOT=/home/ubuntu/aws-config/production-rpc
+# This script assumes an Ubuntu 11.10 server.
+# This git repository should be cloned into $HOME/aws-config.
+export REPO_ROOT=$HOME/aws-config/production-rpc
 export SCRIPT_ROOT=$REPO_ROOT/scripts
 
 # Start out by installing package dependencies
@@ -17,8 +17,11 @@ sudo sed -i 's/8080/9001/g' /var/lib/tomcat6/conf/server.xml
 # Copy configuration data
 sudo ln -s $REPO_ROOT/config/solr_tomcat_context.xml /var/lib/tomcat6/conf/Catalina/localhost/solr.xml
 
+# Create symlink to Solr app directory outside of $HOME
+sudo ln -s $REPO_ROOT/solr /var/lib/tomcat6/khan-solr
+
 # Set permissions so that Tomcat can read the files
-sudo chown -R tomcat6:tomcat6 solr
+sudo chown -R tomcat6:tomcat6 $REPO_ROOT/solr
 
 # Delete the default Tomcat index page
 sudo rm /var/lib/tomcat6/webapps/ROOT/index.html
@@ -37,5 +40,7 @@ sudo /etc/init.d/lighttpd restart
 # Initialize the search index with the latest data
 $SCRIPT_ROOT/update_solr.bash
 
-# Set up a cron job to update the index every hour
-sudo ln -s $SCRIPT_ROOT/update_solr_wrapper.bash /etc/cron.hourly/update_solr
+# Create a local user crontab
+echo "MAILTO=tom@khanacademy.org" > /tmp/crontab
+echo "0 * * * * * $SCRIPT_ROOT/cronic.bash $SCRIPT_ROOT/update_solr.bash" >> /tmp/crontab
+crontab /tmp/crontab
