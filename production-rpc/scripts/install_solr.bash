3a110f97c6a8added9ef718156db7c627aada702
Tom W. Yedwab <tom@khanacademy.org>
2012-05-16 13:43:09 -0700

    Fixes from previous GitHub pull request (in production-rpc-webserver)
    - Instead of packaging the Solr war file, download it during install.
    - Pipe crontab file directly from script & log to new admin email.

--- production-rpc/scripts/install_solr.bash
+++ production-rpc/scripts/install_solr.bash
@@ -7,12 +7,23 @@
 # This git repository should be cloned into $HOME/aws-config.
 export REPO_ROOT=$HOME/aws-config/production-rpc
 export SCRIPT_ROOT=$REPO_ROOT/scripts
+export SOLR_VERSION=3.6.0
+
+# chdir to $REPO_ROOT
+cd $REPO_ROOT
 
 # Start out by installing package dependencies
 sudo apt-get install lighttpd tomcat6 curl mailutils moreutils
 
 # Configure Tomcat: Set port number to 9001
 sudo sed -i 's/8080/9001/g' /var/lib/tomcat6/conf/server.xml
+
+# Download and extract the Solr .war file
+curl http://apache.tradebit.com/pub/lucene/solr/$SOLR_VERSION/apache-solr-$SOLR_VERSION.tgz > $REPO_ROOT/apache-solr.tgz
+tar -xf $REPO_ROOT/apache-solr.tgz
+cp apache-solr-$SOLR_VERSION/dist/apache-solr-$SOLR_VERSION.war $REPO_ROOT/solr/solr.war
+rm -rf apache-solr-$SOLR_VERSION
+rm $REPO_ROOT/apache-solr.tgz
 
 # Copy configuration data
 sudo ln -s $REPO_ROOT/config/solr_tomcat_context.xml /var/lib/tomcat6/conf/Catalina/localhost/solr.xml
@@ -41,6 +52,7 @@ sudo /etc/init.d/lighttpd restart
 $SCRIPT_ROOT/update_solr.bash
 
 # Create a local user crontab
-echo "MAILTO=tom@khanacademy.org" > /tmp/crontab
-echo "0 * * * * $SCRIPT_ROOT/cronic.bash $SCRIPT_ROOT/update_solr.bash" >> /tmp/crontab
-crontab /tmp/crontab
+cat <<EOF | crontab -
+MAILTO=production-rpc-admin+search-cron@khanacademy.org
+0 * * * * $SCRIPT_ROOT/cronic.bash $SCRIPT_ROOT/update_solr.bash
+EOF
